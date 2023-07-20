fae149ba152b4725e73c7aada40327606c198411
arkin99 <115459083+arkin99@users.noreply.github.com>
2023-07-20 13:01:23 +0700

    Add files via upload

---
+++ src/main/java/arkin/tutorial/TutorialMod.java
@@ -0,0 +1,56 @@
+package arkin.tutorial;
+
+import arkin.tutorial.blockEntities.ModBlockEntities;
+import arkin.tutorial.blocks.ModBlocks;
+import arkin.tutorial.items.ModCreativeTabs;
+import arkin.tutorial.items.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TutorialMod.MODID)
+public class TutorialMod {
+    public static final String MODID = "tutorialmod";
+
+    public TutorialMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        bus.addListener(this::commonSetup);
+
+        ModBlocks.BLOCKS.register(bus);
+        ModItems.ITEMS.register(bus);
+        ModBlockEntities.TILES.register(bus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        bus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.COMBAT)
+            event.accept(ModItems.ITEM);
+        if (event.getTab() == ModCreativeTabs.tab) {
+            event.accept(ModItems.ITEMM);
+            event.accept(ModItems.TUT_BLOCK);
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
---
+++ src/main/java/arkin/tutorial/items/ModItems.java
@@ -0,0 +1,21 @@
+package arkin.tutorial.items;
+
+import arkin.tutorial.TutorialMod;
+import arkin.tutorial.blocks.ModBlocks;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MODID);
+
+    public static final RegistryObject<Item> ITEM = ITEMS.register("hh", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ITEMM = ITEMS.register("hhh", BB::new);
+    public static final RegistryObject<Item> TUT_FOOD = ITEMS.register("tut_food", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(19).saturationMod(0.3F).build())));
+    
+    public static final RegistryObject<Item> TUT_BLOCK = ITEMS.register("gg", () -> new BlockItem(ModBlocks.TUT_BLOCK.get(), new Item.Properties()));
+}
